749e97211a26bbc4b306c8ec83bc7c00a1345726
Tevy Jacobs-Gomes <tevy.jacobs-gomes@liveramp.com>
2023-11-26 13:49:32 -0800

    Added columbus street location and address grid

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -5,14 +5,23 @@ import './Address.css';
 function Address() {
   return (
     <section id="Address">
-      <h2>Location</h2>
+      <h2>Locations</h2>
       <div className="AddressContainer">
         <address>
           <a className="Address" target="_blank" rel="noreferrer" href="https://goo.gl/maps/qX7pHNrtavGphEDr6">
+            <p className='StoreName'>Compton's Pacific Heights</p>
             <p>1910 Fillmore Street</p>
             <p>San Francisco, California</p>
           </a>
           <p><a className="PhoneNumber" href="tel:4154166173">415.416.6173</a></p>
+        </address>
+        <address>
+          <a className="Address" target="_blank" rel="noreferrer" href="https://maps.app.goo.gl/r2gq5qNFPPwpZCR1A">
+            <p className='StoreName'>Compton's North Beach</p>
+            <p>543 Columbus Avenue</p>
+            <p>San Francisco, California</p>
+          </a>
+          <p><a className="PhoneNumber" href="tel:6282014198">628.201.4198</a></p>
         </address>
       </div>
     </section>
